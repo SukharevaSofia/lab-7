bb419623b344e256a29ba8ac865c5f4b1be120de
sukha <334641@niuitmo.ru>
2022-06-11 22:03:37 +0300

    meow meow

--- client/src/main/kotlin/client/ClientApp.kt
+++ client/src/main/kotlin/client/ClientApp.kt
@@ -3,16 +3,15 @@ package client
 
 import client.Managers.CommandFinder
 import general.AppIO.InputData
-
 /**
  * @author Sukhareva Sofya
  */
-
 fun main(){
 
     val inputData = InputData()
     val commandFinder = CommandFinder(inputData)
     val client = Client(commandFinder,  7001, "savikin.ru");
     client.run()
+
 
 }
